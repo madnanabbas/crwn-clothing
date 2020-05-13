a44a9a14a9887d026fed27df65af93cbc779c0e6
Adnan Abbas <madnanabbas14@gmail.com>
2020-05-13 13:56:30 +0500

    Updates

---
+++ precache-manifest.0f6cebbc86e02d63c15d1757d66dc8db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7ee5e4c74ef5df483011379bd46bbe",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "cdbc96adb657195c7414",
+    "url": "/crwn-clothing/static/css/main.be8c3796.chunk.css"
+  },
+  {
+    "revision": "ef7d9dcdbf407d411ace",
+    "url": "/crwn-clothing/static/js/2.88bcc801.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/crwn-clothing/static/js/2.88bcc801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbc96adb657195c7414",
+    "url": "/crwn-clothing/static/js/main.61182280.chunk.js"
+  },
+  {
+    "revision": "d972688a51bf970073a7",
+    "url": "/crwn-clothing/static/js/runtime-main.824fe963.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
